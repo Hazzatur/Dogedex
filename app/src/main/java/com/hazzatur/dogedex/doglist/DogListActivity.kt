bf5630f308659611bf07eea899e5cded2a3b6077
Hazzatur <hazzatur@outlook.com>
2023-04-04 10:37:16 -0600

    Create starter project

---
+++ app/src/main/java/com/hazzatur/dogedex/doglist/DogListActivity.kt
@@ -0,0 +1,28 @@
+package com.hazzatur.dogedex.doglist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.hazzatur.dogedex.databinding.ActivityDogListBinding
+
+class DogListActivity : AppCompatActivity() {
+
+    private val dogListViewModel: DogListViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityDogListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val recycler = binding.dogRecycler
+        recycler.layoutManager = LinearLayoutManager(this)
+
+        val adapter = DogAdapter()
+        recycler.adapter = adapter
+
+        dogListViewModel.dogList.observe(this) { dogList ->
+            adapter.submitList(dogList)
+        }
+    }
+}
