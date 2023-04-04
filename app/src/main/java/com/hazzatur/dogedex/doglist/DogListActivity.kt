6ac3af75232e802b910ea04c9ccd70f02c207480
Hazzatur <hazzatur@outlook.com>
2023-04-04 12:03:22 -0600

    Add dog detail activity

--- app/src/main/java/com/hazzatur/dogedex/doglist/DogListActivity.kt
+++ app/src/main/java/com/hazzatur/dogedex/doglist/DogListActivity.kt
@@ -1,10 +1,13 @@
 package com.hazzatur.dogedex.doglist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.hazzatur.dogedex.databinding.ActivityDogListBinding
+import com.hazzatur.dogedex.dogdetail.DogDetailActivity
+import com.hazzatur.dogedex.dogdetail.DogDetailActivity.Companion.DOG_KEY
 
 class DogListActivity : AppCompatActivity() {
 
@@ -19,6 +22,12 @@ class DogListActivity : AppCompatActivity() {
         recycler.layoutManager = LinearLayoutManager(this)
 
         val adapter = DogAdapter()
+
+        adapter.setOnItemClickListener {
+            val intent = Intent(this, DogDetailActivity::class.java)
+            intent.putExtra(DOG_KEY, it)
+            startActivity(intent)
+        }
         recycler.adapter = adapter
 
         dogListViewModel.dogList.observe(this) { dogList ->
