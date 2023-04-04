d0e46c11a73909d430a84ca28d244e9ea4f1436e
Hazzatur <hazzatur@outlook.com>
2023-04-04 11:07:17 -0600

    Replace local dogs list with API call

---
+++ app/src/main/java/com/hazzatur/dogedex/api/responses/DogListResponse.kt
@@ -0,0 +1,7 @@
+package com.hazzatur.dogedex.api.responses
+
+import com.hazzatur.dogedex.Dog
+
+class DogListResponse(
+    val dogs: List<Dog>
+)
--- app/src/main/java/com/hazzatur/dogedex/doglist/DogRepository.kt
+++ app/src/main/java/com/hazzatur/dogedex/doglist/DogRepository.kt
@@ -1,6 +1,7 @@
 package com.hazzatur.dogedex.doglist
 
 import com.hazzatur.dogedex.Dog
+import com.hazzatur.dogedex.api.DogsApi.retrofitService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -8,54 +9,12 @@ class DogRepository {
 
     suspend fun downloadDogs(): List<Dog> {
         return withContext(Dispatchers.IO) {
-            getFakeDogs()
+            val response = retrofitService.getAllDogs()
+            if (response.isSuccess) {
+                response.data.dogs
+            } else {
+                emptyList()
+            }
         }
-    }
-
-    private fun getFakeDogs(): MutableList<Dog> {
-        val dogList = mutableListOf<Dog>()
-        dogList.add(
-            Dog(
-                1, 1, "Chihuahua", "Toy", 5.4,
-                6.7, "", "12 - 15", "", 10.5,
-                12.3
-            )
-        )
-        dogList.add(
-            Dog(
-                2, 1, "Labrador", "Toy", 5.4,
-                6.7, "", "12 - 15", "", 10.5,
-                12.3
-            )
-        )
-        dogList.add(
-            Dog(
-                3, 1, "Retriever", "Toy", 5.4,
-                6.7, "", "12 - 15", "", 10.5,
-                12.3
-            )
-        )
-        dogList.add(
-            Dog(
-                4, 1, "San Bernardo", "Toy", 5.4,
-                6.7, "", "12 - 15", "", 10.5,
-                12.3
-            )
-        )
-        dogList.add(
-            Dog(
-                5, 1, "Husky", "Toy", 5.4,
-                6.7, "", "12 - 15", "", 10.5,
-                12.3
-            )
-        )
-        dogList.add(
-            Dog(
-                6, 1, "Xoloscuincle", "Toy", 5.4,
-                6.7, "", "12 - 15", "", 10.5,
-                12.3
-            )
-        )
-        return dogList
     }
 }
